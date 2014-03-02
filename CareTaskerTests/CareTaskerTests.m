23d9d8220104f51bc1c6a395a0fec81557c0dd53
Jeff Wang <jeffreywang93@gmail.com>
2014-03-01 18:50:56 -0800

    Initial Commit

---
+++ CareTaskerTests/CareTaskerTests.m
@@ -0,0 +1,34 @@
+//
+//  CareTaskerTests.m
+//  CareTaskerTests
+//
+//  Created by Jeffrey Wang on 3/1/14.
+//  Copyright (c) 2014 Jeffrey Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CareTaskerTests : XCTestCase
+
+@end
+
+@implementation CareTaskerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
